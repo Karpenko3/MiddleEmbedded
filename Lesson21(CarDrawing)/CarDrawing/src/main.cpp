4344a5b206f6c8836b8d0ba313c85ce8cfa6914e
Артем <akarpenko134@gmail.com>
2026-02-03 11:30:41 +0200

    Lesson 23 done

---
+++ Lesson21(CarDrawing)/CarDrawing/src/main.cpp
@@ -0,0 +1,65 @@
+#include <Arduino.h>
+#include <GameBoy.h> 
+
+GameBoy gb;
+
+void enemyCar(int x, int y);
+void playerCar(int x, int y);
+void clearPlayerCar(int x, int y);
+void clearEnemyCar(int x, int y);
+
+void setup() {
+  gb.begin(2); 
+}
+
+void loop() {
+  if (gb.getKey() == 4) { // Рух вліво
+    clearPlayerCar(5, 12);
+    playerCar(2, 12);
+  } 
+  else if (gb.getKey() == 5) { // Рух вправо
+    clearPlayerCar(2, 12);
+    playerCar(5, 12);
+  }
+}
+
+
+void playerCar(int x, int y) {
+  gb.drawPoint(x, y);
+  gb.drawPoint(x, y + 1);
+  gb.drawPoint(x - 1, y + 1);
+  gb.drawPoint(x + 1, y + 1);
+  gb.drawPoint(x, y + 2);
+  gb.drawPoint(x - 1, y + 3);
+  gb.drawPoint(x + 1, y + 3);
+}
+
+void enemyCar(int x, int y) {
+  gb.drawPoint(x, y);
+  gb.drawPoint(x, y - 1);
+  gb.drawPoint(x - 1, y - 1);
+  gb.drawPoint(x + 1, y - 1);
+  gb.drawPoint(x, y - 2);
+  gb.drawPoint(x - 1, y - 3);
+  gb.drawPoint(x + 1, y - 3);
+}
+
+void clearPlayerCar(int x, int y) {
+  gb.wipePoint(x, y);
+  gb.wipePoint(x, y + 1);
+  gb.wipePoint(x - 1, y + 1);
+  gb.wipePoint(x + 1, y + 1);
+  gb.wipePoint(x, y + 2);
+  gb.wipePoint(x - 1, y + 3);
+  gb.wipePoint(x + 1, y + 3);
+}
+
+void clearEnemyCar(int x, int y) {
+  gb.wipePoint(x, y);
+  gb.wipePoint(x, y - 1);
+  gb.wipePoint(x - 1, y - 1);
+  gb.wipePoint(x + 1, y - 1);
+  gb.wipePoint(x, y - 2);
+  gb.wipePoint(x - 1, y - 3);
+  gb.wipePoint(x + 1, y - 3);
+}
